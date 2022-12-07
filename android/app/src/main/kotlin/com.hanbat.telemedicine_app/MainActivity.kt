8b203daf99f233cc22cf683e57c7cdff99b09ad9
JihoJeong <65143726+JihoJeong97@users.noreply.github.com>
2022-12-07 23:25:39 +0900

    Delete android directory

--- android/app/src/main/kotlin/com.hanbat.telemedicine_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hanbat.telemedicine_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/hanbat/telemedicine_app2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hanbat.telemedicine_app2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
